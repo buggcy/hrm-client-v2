f54aa90bb5ecb39e5547e6ffe2afb9a377bf919e
Yevhenii Petrenko <54939386+vekaev@users.noreply.github.com>
2024-06-27 13:28:26 -0700

    yevhenii/preview (#38)
    * fix prettier, update packages, and add code view component
    
    * add code component
    
    * fix readme
    
    * complete preview component
    
    * video list
    
    * video list
    
    * fix query keys
    
    * dozen of fixes
    
    * add background
    
    * fix styling
    
    * add background
    
    * fix code preview and prod default replica
    
    * add node env
    
    * fix logging
    
    * fix env var

--- src/constants/env.constants.ts
+++ src/constants/env.constants.ts
@@ -1,7 +1,7 @@
 export const RQH_API_BASE_URL = process.env.NEXT_PUBLIC_RQH_API_BASE_URL;
 export const PORTAL_API_BASE_URL = process.env.NEXT_PUBLIC_PORTAL_API_BASE_URL;
 
-export const isProd = process.env.NODE_ENV === 'production';
+export const isProd = process.env.APP_ENV === 'production';
 
 export const SENTRY_DSN = process.env.NEXT_PUBLIC_SENTRY_DSN;
 export const LOG_ROCKET_ID = process.env.NEXT_PUBLIC_LOG_ROCKET_ID;
