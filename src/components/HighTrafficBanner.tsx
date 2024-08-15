5219de8627d2076d197ba6d3d5504ca629b069bc
NikitaPFE <43850394+NikitaPFE@users.noreply.github.com>
2024-08-15 12:34:20 -0700

    chore: Update HighTrafficBanner component to fix typo in comment (#94)
    Co-authored-by: Nikita Puzyrenko <nikita@tavus.com>

--- src/components/HighTrafficBanner.tsx
+++ src/components/HighTrafficBanner.tsx
@@ -4,9 +4,8 @@ export const HighTrafficBanner = () => (
   <div className="flex items-center justify-center bg-primary p-3 text-center text-white">
     <AlertTriangle className="mr-1.5 inline-block size-5" />
     <p>
-      We are experiencing extremely high traffic to our digital twins. They
-      {/* eslint-disable-next-line react/no-unescaped-entities */}
-      need a little breather, but we're working on it!
+      We are experiencing extremely high traffic to our digital twins. They need
+      a little breather, but we&apos;re working on it!
     </p>
   </div>
 );
