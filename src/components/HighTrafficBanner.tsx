8fcf9b8a4d4a22429d609f0a5a3f3fb7c8d94df2
NikitaPFE <43850394+NikitaPFE@users.noreply.github.com>
2024-08-16 12:16:58 -0700

    refactor: Remove HighTrafficBanner component content (#95)
    Co-authored-by: Nikita Puzyrenko <nikita@tavus.com>

--- src/components/HighTrafficBanner.tsx
+++ src/components/HighTrafficBanner.tsx
@@ -1,11 +1,15 @@
-import { AlertTriangle } from 'lucide-react';
+// import { AlertTriangle } from 'lucide-react';
 
-export const HighTrafficBanner = () => (
-  <div className="flex items-center justify-center bg-primary p-3 text-center text-white">
-    <AlertTriangle className="mr-1.5 inline-block size-5" />
-    <p>
-      We are experiencing extremely high traffic to our digital twins. They need
-      a little breather, but we&apos;re working on it!
-    </p>
-  </div>
-);
+export const HighTrafficBanner = () => {
+  return null;
+
+  // return (
+  //   <div className="flex items-center justify-center bg-primary p-3 text-center text-white">
+  //     <AlertTriangle className="mr-1.5 inline-block size-5" />
+  //     <p>
+  //       We are experiencing extremely high traffic to our digital twins. They
+  //       need a little breather, but we&apos;re working on it!
+  //     </p>
+  //   </div>
+  // );
+};
