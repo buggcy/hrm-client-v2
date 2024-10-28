fa3674dc8ec92332a2cdc5cbd51b27c1b93a926a
Abdullah Sajjad <abdullah.sajjad@buggcy.com>
2024-10-28 18:09:22 +0500

    menu changes

--- src/utils/menu/hr.menu.ts
+++ src/utils/menu/hr.menu.ts
@@ -42,23 +42,18 @@ export const hrMenu: MenuItem[] = [
     icon: UserCog2,
     children: [
       {
-        title: 'Manage Events',
+        title: 'Events',
         icon: CalendarCog,
         href: '/hr/manage-events',
       },
-    ],
-  },
-  {
-    title: 'Manage Announcements',
-    icon: Megaphone,
-    children: [
       {
-        title: 'Manage Announcements',
+        title: 'Announcements',
         icon: Megaphone,
         href: '/hr/manage-announcements',
       },
     ],
   },
+
   {
     title: 'Manage Attendance',
     icon: UserCog2,
