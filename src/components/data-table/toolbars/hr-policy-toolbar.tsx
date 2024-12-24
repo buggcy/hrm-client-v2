0fd0da3541f2e0aadc6df7490063e11785465bcb
fssc2 <ahmad.dcy2@gmail.com>
2024-12-24 17:01:22 +0500

    policy and feedback issue solved

--- src/components/data-table/toolbars/hr-policy-toolbar.tsx
+++ src/components/data-table/toolbars/hr-policy-toolbar.tsx
@@ -88,12 +88,13 @@ export function HrPolicyToolbar<TData extends DataTableType>({
             onFilterChange={setFilterValue}
           />
         )}
-        {(isFiltered || searchTerm) && (
+        {(isFiltered || searchTerm || filterValue.length > 0) && (
           <Button
             variant="ghost"
             onClick={() => {
               table.resetColumnFilters();
               onSearch('');
+              setFilterValue([]);
             }}
             className="h-8 px-2 lg:px-3"
           >
