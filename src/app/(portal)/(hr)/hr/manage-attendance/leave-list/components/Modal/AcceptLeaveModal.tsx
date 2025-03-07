e200cfa06733367e60d85a6e11c2d5f1fc77c32e
Farrukh-Mustafa <farukh.mustafa@buggcy.com>
2025-03-07 16:31:38 +0500

    Design Changes

--- src/app/(portal)/(hr)/hr/manage-attendance/leave-list/components/Modal/AcceptLeaveModal.tsx
+++ src/app/(portal)/(hr)/hr/manage-attendance/leave-list/components/Modal/AcceptLeaveModal.tsx
@@ -47,60 +47,62 @@ export default function AcceptLeaveDialog({
   return (
     <>
       <Dialog open={isOpen} onOpenChange={onOpenChange}>
-        <DialogContent className="max-w-[400px]">
+        <DialogContent className="max-w-[500px]">
           <DialogHeader>
-            <DialogTitle>Accept Leave Request</DialogTitle>
+            <DialogTitle className="mb-4">Accept Leave Request</DialogTitle>
             <DialogDescription>
-              <ScrollArea className="h-[250px] px-4 sm:h-[500px]">
-                {leaveDistribution.map((leave, index) => (
-                  <div
-                    key={index}
-                    className="mb-4 flex flex-row items-center justify-between"
-                  >
-                    <p className="flex flex-row items-center gap-2 text-base text-foreground">
-                      <Badge
-                        variant="outline"
-                        className="flex w-[40px] items-center justify-center"
-                      >
-                        {leave.date.toLocaleDateString('en-US', {
-                          weekday: 'short',
-                        })}
-                      </Badge>
-                      {leave.date.toLocaleDateString('en-CA')}{' '}
-                    </p>
-                    <Select
-                      defaultValue={
-                        isAnnualLeave
-                          ? leave.isAnnual && leave.isPaid
-                            ? 'annual'
-                            : 'unpaid'
-                          : leave.isPaid && leave.isAnnual
-                            ? 'annual'
-                            : leave.isPaid && !leave.isAnnual
-                              ? 'paid'
-                              : 'unpaid'
-                      }
-                      onValueChange={value =>
-                        onChange(
-                          index,
-                          value === 'paid' || value === 'annual',
-                          value === 'annual',
-                        )
-                      }
+              <ScrollArea className="h-[250px] sm:h-[500px]">
+                <div className="size-full p-4">
+                  {leaveDistribution.map((leave, index) => (
+                    <div
+                      key={index}
+                      className="mb-4 flex flex-row items-center justify-between"
                     >
-                      <SelectTrigger className="max-w-[120px] text-foreground">
-                        <SelectValue className="text-current" />
-                      </SelectTrigger>
-                      <SelectContent>
-                        {!isAnnualLeave && (
-                          <SelectItem value="paid">Paid</SelectItem>
-                        )}
-                        <SelectItem value="unpaid">Unpaid</SelectItem>
-                        <SelectItem value="annual">Annual</SelectItem>
-                      </SelectContent>
-                    </Select>
-                  </div>
-                ))}
+                      <p className="flex flex-row items-center gap-2 text-base text-foreground">
+                        <Badge
+                          variant="outline"
+                          className="flex w-[40px] items-center justify-center"
+                        >
+                          {leave.date.toLocaleDateString('en-US', {
+                            weekday: 'short',
+                          })}
+                        </Badge>
+                        {leave.date.toLocaleDateString('en-CA')}{' '}
+                      </p>
+                      <Select
+                        defaultValue={
+                          isAnnualLeave
+                            ? leave.isAnnual && leave.isPaid
+                              ? 'annual'
+                              : 'unpaid'
+                            : leave.isPaid && leave.isAnnual
+                              ? 'annual'
+                              : leave.isPaid && !leave.isAnnual
+                                ? 'paid'
+                                : 'unpaid'
+                        }
+                        onValueChange={value =>
+                          onChange(
+                            index,
+                            value === 'paid' || value === 'annual',
+                            value === 'annual',
+                          )
+                        }
+                      >
+                        <SelectTrigger className="max-w-[170px] text-foreground">
+                          <SelectValue className="text-current" />
+                        </SelectTrigger>
+                        <SelectContent>
+                          {!isAnnualLeave && (
+                            <SelectItem value="paid">Leave</SelectItem>
+                          )}
+                          <SelectItem value="unpaid">Unpaid Leave</SelectItem>
+                          <SelectItem value="annual">Annual Leave</SelectItem>
+                        </SelectContent>
+                      </Select>
+                    </div>
+                  ))}
+                </div>
               </ScrollArea>
             </DialogDescription>
           </DialogHeader>
