00cb7d944498377264ef47ff9897cf332e3d3968
Abdullah Sajjad <abdullah.sajjad@buggcy.com>
2025-04-28 18:58:55 +0500

    add user manual in support + show to hr, employee, manager

--- src/app/(portal)/components/Navigation/components/SupportButton.tsx
+++ src/app/(portal)/components/Navigation/components/SupportButton.tsx
@@ -1,7 +1,11 @@
+import { usePathname, useRouter } from 'next/navigation';
+
 import { CircleHelp } from 'lucide-react';
 
 import { Button, ButtonProps } from '@/components/ui/button';
+import { useStores } from '@/providers/Store.Provider';
 
+import { AuthStoreType } from '@/stores/auth';
 import { cn } from '@/utils';
 
 export const NavigationSupportBtn = ({
@@ -9,13 +13,32 @@ export const NavigationSupportBtn = ({
   className,
   ...props
 }: ButtonProps) => {
+  const { authStore } = useStores() as { authStore: AuthStoreType };
+  const { user } = authStore;
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const handleClick = () => {
+    if (user?.roleId === 1) {
+      router.push('/hr/support');
+    } else if (user?.roleId === 2) {
+      router.push('/employee/support');
+    } else if (user?.roleId === 3) {
+      router.push('/manager/support');
+    }
+  };
   return (
     <Button
       variant="ghost"
       className={cn(
         'h-10 w-full justify-start overflow-hidden pl-2.5 text-muted-foreground transition-all duration-200 group-hover:w-60 sm:size-10',
+        {
+          'bg-primary-foreground text-primary hover:bg-primary-foreground hover:text-primary group-hover:w-60':
+            pathname.includes('/support'),
+        },
         className,
       )}
+      onClick={handleClick}
       {...props}
     >
       <div className="flex w-52 items-center gap-2">
@@ -33,6 +56,20 @@ export const SupportButton = ({
   className,
   ...props
 }: ButtonProps) => {
+  const { authStore } = useStores() as { authStore: AuthStoreType };
+  const { user } = authStore;
+  const router = useRouter();
+
+  const handleClick = () => {
+    if (user?.roleId === 1) {
+      router.push('/hr/support');
+    } else if (user?.roleId === 2) {
+      router.push('/employee/support');
+    } else if (user?.roleId === 3) {
+      router.push('/manager/support');
+    }
+  };
+
   return (
     <Button
       variant="outline"
@@ -40,12 +77,13 @@ export const SupportButton = ({
         'h-10 w-full justify-start overflow-hidden text-muted-foreground',
         className,
       )}
+      onClick={handleClick}
       {...props}
     >
       {children || (
         <>
           <CircleHelp className="size-5" />
-          Support
+          Suppor
         </>
       )}
     </Button>
